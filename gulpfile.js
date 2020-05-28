9d1c49b96778caf18d4755aa5a9cebe300c89ef3
clement <howpines@gmail.com>
2020-05-28 16:27:36 +0800

    新增

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var fileinclude = require('gulp-file-include');
+var imagemin = require('gulp-imagemin');
+var gutil = require( 'gulp-util' );
+var ftp = require( 'vinyl-ftp' );
+var connect = require('gulp-connect-php');
+var reload = browserSync.reload;
 
 gulp.task('hello',function(){
     //do 
@@ -73,7 +78,76 @@ gulp.task('sass', function () {
       }))
       .pipe(gulp.dest('./dest'));
 });
-   
+// 壓圖
+gulp.task('miniimg',function(){
+    gulp.src('./images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dest/images'))
+});
+// ftp
+gulp.task( 'ftp',['miniimg'], function () {
+ 
+    var conn = ftp.create( {
+        host:     '140.115.236.71',
+        user:     '%ed101+',
+        password: '!654=stu&',
+        parallel: 10
+    } );
+ 
+    var globs = [
+        'dest/**',
+        'dest/css/**',
+        'dest/images/**',
+        'index.html'
+    ];
+ 
+    // using base = '.' will transfer everything to /public_html correctly
+    // turn off buffering in gulp.src for best performance
+ 
+    return gulp.src( globs, { base: '.', buffer: false } )
+        .pipe( conn.newer( '/T2000277' ) ) // only upload newer files
+        .pipe( conn.dest( '/T2000277' ) );
+ 
+} );
+// 同步
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dest",
+            index : "index.html"
+        }
+    });
+    gulp.watch('./dev/sass/*.scss' ,['sass']).on('change',reload);
+    gulp.watch(['./dev/*.html' ,'./dev/**/*.html'] ,['fileinclude']).on('change',reload);
+});
+
+//php
+gulp.task('php', function () {
+  connect.server({
+    base: './php',
+    port: 8010,
+    keepalive: true
+  });
+});
+
+
+gulp.task('browserSync',['php'], function() {
+  browserSync.init({
+      //proxy:"localhost:8010",
+      baseDir: "./php",
+      open:true,
+      notify:false
+
+  });
+});
+
+
+gulp.task('devphp', [ 'browserSync'], function() {
+  gulp.watch('./php/*.php', browserSync.reload);
+});
+
+
+// 預設
 gulp.task('default', function() {
     browserSync.init({
         server: {
---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3002/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+<?php echo "<h1> 標題1</h1>" ?>  
+<!-- <?php  phpinfo(); ?> -->
+
+
+</body>
+</html>
